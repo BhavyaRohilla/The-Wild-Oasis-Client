32a9dca3df2b8be822aace795ebe669a47010592
Bhavya Rohilla <officialbhavyarohilla@gmail.com>
2025-09-18 21:13:17 +0530

    api-build complete

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    qualities: [75, 80, 100],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "qlwihtsfhluyqoftfmjt.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/cabin-images/**",
+      },
+    ],
+  },
+  // output: "export",
+};
 
 export default nextConfig;
