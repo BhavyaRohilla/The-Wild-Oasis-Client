e7c0a35b42fb0633ebe42ed4d07abc7ef6309462
Bhavya Rohilla <officialbhavyarohilla@gmail.com>
2025-09-18 09:22:39 +0530

    layout complete

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,4 @@
 import Header from "./_components/Header";
-import Logo from "./_components/Logo";
-import Navigation from "./_components/Navigation";
 
 import "@/app/_styles/globals.css";
 
@@ -24,12 +22,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
       >
         <Header />
 
-        <div className="flex-1 px-8 py-12">
-          <main className="max-w-7xl mx-auto">{children}</main>
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">{children}</main>
         </div>
       </body>
     </html>
--- app/pages/cabins-page.js
+++ app/pages/cabins-page.js
@@ -1,8 +1,4 @@
-import CabinCard from "../_components/CabinCard";
-
-export const metadata = {
-  title: "Cabins",
-};
+import CabinCard from "@/app/_components/CabinCard";
 
 export default function Page() {
   // CHANGE
@@ -17,9 +13,9 @@ export default function Page() {
         Cozy yet luxurious cabins, located right in the heart of the Italian
         Dolomites. Imagine waking up to beautiful mountain views, spending your
         days exploring the dark forests around, or just relaxing in your private
-        hot tub under the stars. Enjoy nature&apos;s beauty in your own little
-        home away from home. The perfect spot for a peaceful, calm vacation.
-        Welcome to paradise.
+        hot tub under the stars. Enjoy nature's beauty in your own little home
+        away from home. The perfect spot for a peaceful, calm vacation. Welcome
+        to paradise.
       </p>
 
       {cabins.length > 0 && (
